a544e26c4bc3c7ce84231cc8bc2c2a72086caddd
BuggyChapri <darkkboy0013@gmail.com>
2025-01-25 08:16:25 +0100

    Is my git s working?

---
+++ Global.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+var spawned = false
---
+++ scripts/main.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+onready var satellite_file = preload("res://scene/satellite.tscn")
+onready var earth = $earth
+
+var velocity = Vector2()
+var angle = 0.0
+var orbit_radius = 150
+var speed = 0.6
+
+var satellite
+
+func _process(delta):
+	if Input.is_action_just_pressed("send"):
+		Global.spawned = true
+		satellite = satellite_file.instance()
+		satellite.global_position = $earth/Position2D.global_position + Vector2(orbit_radius, 0)
+		get_parent().add_child(satellite)
+	
+	if satellite:
+		angle += speed * delta
+		var orbit_position = Vector2(cos(angle), sin(angle)) * orbit_radius
+		satellite.global_position = earth.global_position + orbit_position
