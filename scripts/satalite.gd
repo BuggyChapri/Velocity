c5092f23465ca2c7abf6bc93104b82df8c2b8f4c
BuggyChapri <darkkboy0013@gmail.com>
2025-01-25 13:08:19 +0100

    Setallite collison is working

--- scripts/satalite.gd
+++ scripts/satalite.gd
@@ -4,9 +4,18 @@ onready var earth = get_parent().get_node("earth")
 var speed = 65
 var angle = 0.0
 var radius = 0  
+var collison
+
+var is_colliding
+
+var velocity = Vector2()
 
 func _ready():
+	is_colliding = false
 	radius = Global.radius
+
+func _process(delta):
+	collided()
 
 func _physics_process(delta):
 	if earth:
@@ -15,3 +24,14 @@ func _physics_process(delta):
 		rotation_degrees += speed * delta
 		var orbit_position = Vector2(cos(angle), sin(angle)) * radius
 		global_position = earth.global_position + orbit_position
+
+	if collison:
+		var other_setallite = collison.collider 
+		if other_setallite is KinematicBody2D:  
+			is_colliding = true
+
+	collison = move_and_collide(velocity)
+
+func collided():
+	if is_colliding:
+		print("yty")
